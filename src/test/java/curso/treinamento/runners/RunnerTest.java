ea896b23497647d51d9ef775ce8354a42ce42b17
Lucas Krisiak <lucas.krisiak@hotmail.com>
2019-09-05 09:07:48 -0300

    AULA 4 DIA 5

--- src/test/java/curso/treinamento/runners/RunnerTest.java
+++ src/test/java/curso/treinamento/runners/RunnerTest.java
@@ -1,5 +1,20 @@
 package curso.treinamento.runners;
 
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = {"src/test/resources/features"},
+		glue = {"curso.treinamento.steps"},
+		snippets = SnippetType.UNDERSCORE
+		
+		)
+
+
 public class RunnerTest {
 
 }
---
+++ src/test/resources/features/login.feature
@@ -0,0 +1,8 @@
+#language: pt
+
+	Funcionalidade: Login
+	
+	Cenario: 	Realizar login com sucesso
+		Dado que eu deseje logar no sistema
+		Quando faço login com o usuário "admin@phptravels.com" e senha "demoadmin"
+		Entao sou autenticado com sucesso
